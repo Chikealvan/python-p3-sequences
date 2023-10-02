781d69fb670e4092ad6ea9d609f973dacbc574b1
chikealvan <nwadoziechikealvan@gmail.com>
2023-10-02 01:43:19 +0100

    first commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a, b = 0, 1
+    count = 0
+
+    while count < length:
+        print(a, end=" ") 
+        a, b = b, a + b  
+        count += 1
+    
